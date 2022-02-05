139e0e88758cb983cb30e5e3e8fb853346726875
Akanksha1212 <singhakanksha221b@gmail.com>
2022-02-06 01:32:17 +0530

    Initial commit :rocket:

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:hackviolet/screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    onGenerateRoute: (settings) {
+      switch (settings.name) {
+        case '/':
+          return MaterialPageRoute(builder: (context) => MyApp());
+        case '/home':
+          return MaterialPageRoute(builder: (context) => Home());
+      }
+    },
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 4),
+      () => Navigator.pushReplacementNamed(context, '/home'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+
+        decoration: BoxDecoration(color: Color(0xffFBFFE2)),
+        // child: Image.asset('assets/images/logo.png'),
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage("assets/"),
+            ),
+            Image(
+              image: AssetImage("assets/"),
+              width: 300,
+              height: 300,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
